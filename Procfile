0c6a0a63685830c63dd365788f775aef710f8ed3
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-19 13:25:21 +0530

    railway fix

--- Procfile
+++ Procfile
@@ -1,4 +1,5 @@
 
-web: gunicorn imf_api.wsgi --log-file - 
+web: gunicorn imf_api.imf_api.wsgi:application --bind 0.0.0.0:$PORT --log-file=/dev/stdout
+
 
 web: python manage.py migrate && gunicorn imf_api.wsgi
--- imf_api/imf_api/settings.py
+++ imf_api/imf_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ivfl3785=bigh#d%mn@z-v2y6&o7twp1nomc&jgcxjx0!ujowd'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -70,7 +70,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["postgresu"],
+        'USER': os.environ["PGUSER"],
         'PASSWORD': os.environ["PGPASSWORD"],
         'HOST': os.environ["PGHOST"],
         'PORT': os.environ["PGPORT"],
