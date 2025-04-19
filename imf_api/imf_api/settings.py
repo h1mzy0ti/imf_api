862c79bd4dbe167e81174eb4cd213286d5a6533c
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-19 22:37:09 +0530

    yaml update

--- imf_api/imf_api/settings.py
+++ imf_api/imf_api/settings.py
@@ -66,12 +66,17 @@ REST_FRAMEWORK = {
     ],
 }
 
+from decouple import config
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
 }
 
 
